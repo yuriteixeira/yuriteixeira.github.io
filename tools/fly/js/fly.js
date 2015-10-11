be9d1e438bd53d3fd4495ff63592ea9e38f0ea1f
Yuri Teixeira <oyuriteixeira@gmail.com>
2015-10-11 21:17:08 +0200

    [FLY] Add basic core methods

--- tools/fly/js/fly.js
+++ tools/fly/js/fly.js
@@ -1,9 +1,42 @@
-var Fly = function (console) {
-  this.console = console;
+var Fly = function () {};
+
+Fly.prototype.addDaysToDate = function (startDate, durationDays) {
+  var startMils = startDate.getTime();
+  var realDurationDays = durationDays - 1;
+  var realDurationDaysMils = realDurationDays * 24 * 60 * 60 * 1000;
+  var returnDateMils = startMils + realDurationDaysMils;
+  var returnDate = new Date(returnDateMils);
+  return returnDate;
 };
 
-Fly.prototype.start = function () {
-  this.console.log('FLY >>> started');
+Fly.prototype.formatDateForSearchUrl = function (date) {
+  var twoDigitsYear = String(date.getFullYear()).slice(-2);
+  var month = String(date.getMonth() + 1);
+  var twoDigitsMonth = month.length > 1 ? month : "0" + month;
+  var day = String(date.getDate());
+  var twoDigitsDay = day.length > 1 ? day : "0" + day;
+  var formattedDate = twoDigitsYear + twoDigitsMonth + twoDigitsDay;
+  return formattedDate;
 };
+
+Fly.prototype.getSearchUrls = function (startDate, durationDays, variationDays, origin, destiny) {
+  var urls = [];
+
+  for (var i = 0; i < variationDays; i++) {
+    var iterStartDate = this.addDaysToDate(startDate, i + 1);
+    var iterReturnDate = this.addDaysToDate(iterStartDate, durationDays);
+    var templateUrl = "http://www.skyscanner.com/transport/flights/%o/%d/%s/%e";
+    var url = templateUrl
+      .replace("%o", origin)
+      .replace("%d", destiny)
+      .replace("%s", this.formatDateForSearchUrl(iterStartDate))
+      .replace("%e", this.formatDateForSearchUrl(iterReturnDate))
+    ;
+    urls.push(url);
+  }
+
+  return urls;
+};
+
 
 module.exports = Fly;
--- tools/fly/spec/fly.spec.js
+++ tools/fly/spec/fly.spec.js
@@ -1,13 +1,40 @@
 describe("Fly", function () {
   var Fly = require("../js/fly");
 
-  it("should call console.log, when started", function () {
-    var console = jasmine.createSpyObj("console", ["log"]);
-    var fly = new Fly(console);
+  it("calculate the return date, given a start and duration", function () {
+    var startDate = new Date("2015-01-01");
+    var durationDays = 7;
 
-    fly.start();
+    var fly = new Fly();
+    var result = fly.addDaysToDate(startDate, durationDays);
 
-    expect(console.log.calls.count()).toBe(1);
-    expect(console.log.calls.argsFor(0)[0]).toBe("FLY >>> started");
+    expect(result.getDate()).toEqual(7);
+    expect(result.getMonth()).toEqual(0);
+    expect(result.getFullYear()).toEqual(2015);
+  });
+
+  it("format date for the search url", function () {
+    var date = new Date("2015-01-25");
+
+    var fly = new Fly();
+    var result = fly.formatDateForSearchUrl(date);
+
+    expect(result).toEqual("150125");
+  });
+
+  it("return search urls, given a start, duration, variation, origin & destiny", function () {
+    var startDate = new Date("2015-01-01");
+    var durationDays = 7;
+    var variationDays = 3;
+    var origin = "stoc";
+    var destiny = "saoa";
+
+    var fly = new Fly();
+    var result = fly.getSearchUrls(startDate, durationDays, variationDays, origin, destiny);
+
+    expect(result.length).toEqual(3);
+    expect(result).toContain("http://www.skyscanner.com/transport/flights/stoc/saoa/150101/150107");
+    expect(result).toContain("http://www.skyscanner.com/transport/flights/stoc/saoa/150102/150108");
+    expect(result).toContain("http://www.skyscanner.com/transport/flights/stoc/saoa/150103/150109");
   });
 });
